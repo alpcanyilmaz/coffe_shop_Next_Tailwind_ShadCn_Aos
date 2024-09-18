9e1e2125778589e0581ea149de410b58275aeb05
alpcanyilmaz <alpcanyilmazz@gmail.com>
2024-09-18 16:20:43 +0300

    added container on tailwind config

---
+++ app/_components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <>
+      <div className="container">
+        <h1>hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default Banner;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./_components/Header";
+import Banner from "./_components/Banner";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Banner />
     </>
   );
 }
